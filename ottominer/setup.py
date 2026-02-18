b17980bc18e01fc77954a35254e26ef11691560c
Fatih Burak Karagöz <fatihburak@pm.me>
2026-02-18 16:53:05 +0300

    fix: update console_scripts entry point to cli.main

--- ottominer/setup.py
+++ ottominer/setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
+
 class PostInstallCommand(install):
     def run(self):
         install.run(self)
-        # Import and run after installation
         from ottominer import setup
+
         setup()
 
+
 setup(
-    name='ottominer',
-    version='0.1',
+    name="ottominer",
+    version="0.1",
     packages=find_packages(),
     cmdclass={
-        'install': PostInstallCommand,
+        "install": PostInstallCommand,
     },
     entry_points={
-        'console_scripts': [
-            'ottominer=ottominer.main:main',
+        "console_scripts": [
+            "ottominer=ottominer.cli.main:main",
         ],
     },
 )
